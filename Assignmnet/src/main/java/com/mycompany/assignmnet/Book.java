63258085083ef8e693576a7c1f7711acccf71d41
Jonathan Borg <jonathan.borg.14@um.edu.mt>
2016-04-13 10:45:11 +0200

    Shell code for initial testing of unit tests

---
+++ Assignmnet/src/main/java/com/mycompany/assignmnet/Book.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.assignmnet;
+
+import java.util.*;
+/**
+ *
+ * @author jonathan
+ */
+public class Book {
+    private String title;
+    private String author;
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+}
+
